53f9412c3e1f2347e395d76a1478bb5c694af288
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-08-11 23:24:30 +0200

    test: fix lint

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,6 @@ import assert from "node:assert/strict";
 // eslint-disable-next-line n/no-unsupported-features/node-builtins
 import { it } from "node:test";
 
-it("should be ok", () => {
+await it("should be ok", () => {
   assert.equal("ok", "ok");
 });
